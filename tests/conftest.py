a0ec6dccd3bdcac17014df25f674f10ce6ebacbb
Emily <esfendler@gmail.com>
2020-09-22 21:03:43 -0400

    added tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,21 @@
 # Define test fixtures here.
+
+@pytest.fixture
+def success_response():
+    return {
+        "input_string": "123 main st chicago il",
+        "address_components": {
+            "AddressNumber":"123",
+            "StreetName":"main",
+            "StreetNamePostType":"st",
+            "PlaceName":"chicago",
+            "StateName":"il"
+        },
+        "address_type": "Street Address",
+    }
+
+@pytest.fixture
+def error_response():
+    return {
+        "error": "Unable to parse this address due to repeated labels."
+    }
--- tests/test_views.py
+++ tests/test_views.py
@@ -1,15 +1,25 @@
 import pytest
 
 
-def test_api_parse_succeds(client):
+def test_api_parse_succeeds(client, success_response):
     # TODO: Finish this test. Send a request to the API and confirm that the
     # data comes back in the appropriate format.
     address_string = '123 main st chicago il'
-    pytest.fail()
+    response = client.get('/api/parse/', {'address': address_string })
+
+    assert response.status_code == 200
+    assert response.json() == success_response
+    # pytest.fail()
 
 
-def test_api_parse_raises_error(client):
+def test_api_parse_raises_error(client, error_response):
     # TODO: Finish this test. The address_string below will raise a
     # RepeatedLabelError, so ParseAddress.parse() will not be able to parse it.
     address_string = '123 main st chicago il 123 main st'
-    pytest.fail()
+
+    response = client.get('/api/parse/', {'address': address_string })
+
+    assert response.status_code == 400
+    assert response.json() == error_response
+
+    # pytest.fail()
